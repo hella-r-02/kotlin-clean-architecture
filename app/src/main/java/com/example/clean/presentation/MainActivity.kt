0a72250d1b2074c849eb76a91259d78ef69b814f
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-07-24 14:09:07 +0300

    Modules

--- app/src/main/java/com/example/clean/presentation/MainActivity.kt
+++ app/src/main/java/com/example/clean/presentation/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.EditText
 import android.widget.TextView
 import com.example.clean.R
 import com.example.clean.data.repository.UserRepositoryImpl
-import com.example.clean.data.storage.UserStorage
 import com.example.clean.data.storage.sharedprefs.SharedPrefUserStorage
 import com.example.clean.domain.models.SaveUserNameParam
 import com.example.clean.domain.models.UserName
